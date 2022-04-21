b874bd0561d93746d03492815a0e43fd1c404654
Ted <langhorst@unc.edu>
2022-04-21 13:22:47 -0400

    Cleanup

--- EarthEngine/SCREAMinG_iterate.py
+++ EarthEngine/SCREAMinG_iterate.py
@@ -3,26 +3,17 @@
 """
 Created on Wed May 13 12:13:13 2020
 
-@author: Ted
-
-
-TODO
-- check all angle reference/orientation
-- implement circular derivative for curvature
-- cutoff detection (still water, lost connection)
+Ted Langhorst
+tlang@live.unc.edu
 
 """
 
 import ee
-import csv
 import time
 import numpy as np
-import pandas as pd
 ee.Initialize()  
 
-
 pixMap = ee.Image("users/tedlanghorst/SWORD_pixmap_v09")
-
 
 allReaches = (ee.FeatureCollection('users/tedlanghorst/SWORD_reach_v09')
              .filterMetadata('width','greater_than',150)
@@ -32,30 +23,7 @@ riverReachFilter = filter(lambda r: r%10==1, allReaches)
 reachList = list(riverReachFilter)
 
 
-# pixMap_reachList = [] 
-# with open('/Users/Ted/GDrive/SWORD/SWORD_reachList_river_meanGt150.csv') as csvDataFile:
-#     csvReader = csv.reader(csvDataFile)
-#     next(csvReader) #skip header
-#     for row in csvReader:
-#         pixMap_reachList.append(int(row[0]))
-
-# v1v8 = pd.read_csv('/Users/Ted/Documents/SWORD_v09/Version_Differences/diff/reachDifferences.csv')
-
-# reachList_df = v1v8[v1v8['v01'].isin(pixMap_reachList)]
-# reachList = list(reachList_df['v08'])
-
-# Mississippi
-# R1 = 74000000000
-# R2 = 75000000000
-
-## Yukon
-# R1 = 81200000000
-# R2 = 81300000000
-
-
-
-
-## NA
+#reach limits and step
 RStart = 10000000000
 REnd = 100000000000
 dR = 1000000000
@@ -129,57 +97,4 @@ for idx in range(len(reachBounds)):
   now = time.localtime()
   print("{0:02}:{1:02}\t submitting {2} reaches from range {3}:{4}".format(
     now.tm_hour,now.tm_min,len(reachListFilt),R1,R2))
-  # ft.taskMonitor(exportTask, 30)
-  # now = time.localtime()
-  # print("{0:02}:{1:02} finished range {2}:{3}".format(now.tm_hour,now.tm_min,R1,R2))
-
-
-"""
-failed reaches. 2/12/2021
-v09 run 1
-35000000000_36000000000_2000_08_03
-Error: Number of pixels requested from Window.max exceeds the maximum allowed (2^31)
-
-
-
-Yukon
-81210300021
-81210300041
-81210300051
-
-5/22/20
-81210900401
-81210900211
-81210700081
-81210500121
-
-81210900201
-"""
-
-"""
-failed tasks using landsatMask method
-74225000900_74225001000_2000_15_04
-74241301600_74241301700_2000_15_04
-74249100200_74249100300_2000_15_04
-74252100400_74252100500_2000_15_04
-
-"""
-
-"""
-
-exportTask = ee.batch.Export.image.toDrive(
-  image = mData.toFloat(),
-  description = "mData",
-  scale = SCALE,
-  crs = CRS,
-  maxPixels = int(1E13)).start()
-
-
-
-
-
-
-
-
-
-"""
+  ft.taskMonitor(exportTask, 10)
